1d8ee91c9e3f36ce467f6c6567eb34921ba88369
julian.thomas <juliancthomas@gmail.com>
2026-02-18 14:34:31 -0500

    fix: transaction fees weren't showing

--- public/templates/registration-form.php
+++ public/templates/registration-form.php
@@ -31,14 +31,15 @@ $captcha_site_key = $captcha_service->get_site_key();
 $captcha_provider = $captcha_service->get_provider();
 
 // Get settings for waiver and fees
-$waiver_text = get_option( 'stsrc_waiver_text', '' );
-$tax_rate = floatval( get_option( 'stsrc_tax_rate', '0' ) );
+$use_acf = function_exists( 'get_field' );
+$waiver_text = $use_acf ? get_field( 'stsrc_waiver_text', 'option' ) : get_option( 'stsrc_waiver_text', '' );
+$tax_rate = floatval( $use_acf ? get_field( 'stsrc_tax_rate', 'option' ) : get_option( 'stsrc_tax_rate', '0' ) );
 $transaction_fees = array(
-	'card' => get_option( 'stsrc_fee_card', '' ),
-	'bank_account' => get_option( 'stsrc_fee_bank_account', '' ),
-	'zelle' => get_option( 'stsrc_fee_zelle', '' ),
-	'check' => get_option( 'stsrc_fee_check', '' ),
-	'pay_later' => get_option( 'stsrc_fee_pay_later', '' ),
+	'card'         => $use_acf ? get_field( 'stsrc_fee_card', 'option' ) : get_option( 'stsrc_fee_card', '' ),
+	'bank_account' => $use_acf ? get_field( 'stsrc_fee_bank_account', 'option' ) : get_option( 'stsrc_fee_bank_account', '' ),
+	'zelle'        => $use_acf ? get_field( 'stsrc_fee_zelle', 'option' ) : get_option( 'stsrc_fee_zelle', '' ),
+	'check'        => $use_acf ? get_field( 'stsrc_fee_check', 'option' ) : get_option( 'stsrc_fee_check', '' ),
+	'pay_later'    => $use_acf ? get_field( 'stsrc_fee_pay_later', 'option' ) : get_option( 'stsrc_fee_pay_later', '' ),
 );
 $extra_member_fee = 50.00; // Fee per extra member
 
